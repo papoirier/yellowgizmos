fc78d6a984cc26e695d12773cc64352f123fc9f8
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2015-07-15 14:00:13 -0400

    contact form

--- content-contact.php
+++ content-contact.php
@@ -4,9 +4,15 @@ foreach( $mypages as $page ) {
     $content = $page->post_content;
     $content = apply_filters( 'get_the_content', $content ); ?>
             <?php $url = wp_get_attachment_url( get_post_thumbnail_id($page->ID, 'medium') ); ?>
-            <img src="<?php echo $url; ?>" title="<?php echo $content; ?>" alt="<?php echo $page->post_title; ?>'s photo" data-toggle="tooltip" data-placement="top" class="img-responsive">
-            <h5><?php echo $page->post_title; ?></h5>
-            <p><?php echo $page->post_content; ?></p>
+        	<div class="row mail-phone">
+            	<div class="col-md-3">
+            		<img src="<?php echo $url; ?>" title="<?php echo $content; ?>" alt="<?php echo $page->post_title; ?>'s photo" data-toggle="tooltip" data-placement="top" class="img-responsive">
+                </div>
+                <div class="col-md-9">
+                	<h5><?php echo $page->post_title; ?></h5>
+       				<p><?php echo $page->post_content; ?></p>
+                </div>
+            </div>
             
-
+            
 <?php } ?>
--- page-contact.php
+++ page-contact.php
@@ -1,31 +1,29 @@
 <?php // CONTACT --------------------------------------------- ?>
+<?php 
+/*
+ * mail     -> 129
+ * phone    -> 130
+ */
+?>
 
-<div class="container">
-    <?php if (! is_front_page()) { ?>
-	<div class="row">
-    	<div class="col-md-12 main-title">
-    		<h2><?php the_title();?></h2>
-            <h2><?php the_secondary_title();?></h2>
+<div class="container contact-title">    
+    <div class="row">
+        <div class="col-md-7 ">
+            <h4><?php the_title();?></h4>
+            <h4 class="text-white"><?php the_secondary_title(); ?></h4>
         </div>
     </div>
-    <?php } ?>
-    <?php 
-    /*
-     * mail     -> 129
-     * phone    -> 130
-     */
-    ?>
+</div>
+<div class="container">
     <div class="row">
         <div class="col-md-7 ">
             <div class="contact-form">
-                <h4><?php the_title();?></h4>
-            	<h4 class="text-white"><?php the_secondary_title(); ?></h4>
             	<?php //echo do_shortcode( '[contact-form-7 id="66" title="Contact Yieldmo"]' ); // local ?>
                 <?php echo do_shortcode( '[contact-form-7 id="68" title="Contact Yieldmo"]' ); // server ?>
             </div>
         </div>
         <div class="col-md-4 col-md-offset-1">
-            <?php the_content();?>
+            <div class="text-mail-phone"><?php the_content();?></div>
             <?php require(locate_template('content-contact.php')); ?>
         </div>
     </div>
