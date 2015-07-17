1ee914533acc4eb7a60de97cc01354423e814efc
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2015-07-16 21:07:56 -0400

    intro almost sorta twerkz 2

--- content-intro-details.php
+++ content-intro-details.php
@@ -1,6 +1,4 @@
 <?php $mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'menu_order', 'order' => 'ASC' ) ); ?>
-
-
 <ol class="carousel-indicators">
 	<?php
 $count = 0;
@@ -8,7 +6,7 @@ foreach( $mypages as $page ) {
 	$count++;   
     $content = $page->post_content;
     $content = apply_filters( 'get_the_content', $content ); ?> 
-		<li data-target="#carousel-<?php echo print_the_slug(); ?>" data-slide-to="0" class="<?php if($count == 1){echo ' active';}?>"></li>
+		<li data-target="#carousel-<?php echo print_the_slug(); ?>" data-slide-to="<?php echo $count-1; ?>" class="<?php if($count == 1){echo ' active';}?>"></li>
 	<?php } ?>
 </ol>
 
--- content-intro.php
+++ content-intro.php
@@ -1,10 +1,32 @@
 <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
 <div id="carousel-<?php echo print_the_slug(); ?>" class="carousel slide" data-ride="carousel">
-	
-		
-			<?php require(locate_template('content-intro-details.php')); ?>
-		
-	
+			
+	<?php $mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'menu_order', 'order' => 'ASC' ) ); ?>
+	<ol class="carousel-indicators">
+		<?php
+	$count = 0;
+	foreach( $mypages as $page ) {  
+		$count++;   
+	    $content = $page->post_content;
+	    $content = apply_filters( 'get_the_content', $content ); ?> 
+			<li data-target="#carousel-<?php echo print_the_slug(); ?>" data-slide-to="<?php echo $count-1; ?>" class="<?php if($count == 1){echo ' active';}?>"></li>
+		<?php } ?>
+	</ol>
+
+	<div class="carousel-inner" role="listbox">
+	<?php
+	$count = 0;
+	foreach( $mypages as $page ) {   
+		$count++;   
+	    $content = $page->post_content;
+	    $content = apply_filters( 'get_the_content', $content ); ?>
+
+			<div class="item <?php if($count == 1){echo ' active';}?>">
+	            <?php $url = wp_get_attachment_url( get_post_thumbnail_id($page->ID, 'large') ); ?>
+	        	<img src="<?php echo $url; ?>">
+	        </div>        
+	<?php } ?>
+	</div> <!-- /.carousel-inner -->
 
 	<a class="left carousel-control" href="#carousel-<?php echo print_the_slug(); ?>" role="button" data-slide="prev">
 		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
