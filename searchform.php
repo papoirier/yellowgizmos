5c4460e01e72ccc18eb7f7c8ebd6df663f24aa0e
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2015-07-20 15:44:46 -0400

    styled most of the blog landing page

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
-<form action="/" method="get">
-	<input type="text" name="s" id="search" placeholder="<?php echo esc_attr_x( 'Search…', 'placeholder' ) ?>" value="<?php the_search_query(); ?>" />
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 </form>
