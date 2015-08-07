39a6a6e332215d00a8af37171c0de11e63a94ac0
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2015-08-07 09:52:49 -0400

    job posting page mostly done

--- page.php
+++ page.php
@@ -8,7 +8,6 @@
 ?>
 <?php get_header(); ?>
 
-?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div id="careers">
 	<div class="container">
@@ -27,8 +26,8 @@
 <div class="container">
 	<div class="row">
     	<div class="col-md-8 col-md-offset-2">
-    		<h3><?php the_secondary_title(); ?></h3>
-			<h3 class="regular"><?php the_title(); ?></h3>
+    		<h3 class="text-center"><?php the_secondary_title(); ?></h3>
+			<h3 class="regular text-center"><?php the_title(); ?></h3>
         </div>
     </div>
 </div>
@@ -36,12 +35,47 @@
 <div class="container">
 	<div class="row">
     	<div class="col-md-8 col-md-offset-2">
-    		<p><?php the_content(); ?></p>
+    		<h4 class="regular text-center"><?php the_content(); ?></h4>
         </div>
     </div>
 </div>
 <?php endwhile; endif; ?>
 <hr>
 <?php // job description  ?>
+<div class="container job-description">
+	<div class="row">
+		<?php $mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'menu_order', 'order' => 'ASC' ) ); ?>
+	    <?php
+	    foreach( $mypages as $page ) {      
+	        $content = $page->post_content;
+	        $content = apply_filters( 'get_the_content', $content ); ?>
+	        <div class="col-md-4">
+	            <h4><?php echo $page->post_title; ?></h4>
+	            <p><?php echo $page->post_content; ?></p>
+	        </div>
+	    <?php } ?>
+	    <?php // about yieldmo page -> 1237 ?>
 
+	        <div class="col-md-4">
+	        	<h4><?php echo get_the_title( 1237 ); ?></h4>
+	    		<p><?php echo get_post_field('post_content', 1237); ?></p> 
+	        </div>
+    </div>
+</div>
+<hr>
+<div class="container job-contact">
+	<div class="row">
+    	<div class="col-md-12 text-center">
+    		<img src="<?php echo get_stylesheet_directory_uri() ?>/img/btn-mail.svg" class="img-responsive center-block">
+    		<h5 class="regular">If you’re interested, please send cover letter, website/portfolio, and resume directly to :</h5>
+			<h5>Will Sparks</h5>
+			<h5><script>document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'&'+'#'+'1'+'0'+'9'+';'+'a'+'i'+'l'+'t'+'o'+'&'+'#'+'5'+'8'+';'+
+'w'+'&'+'#'+'3'+'7'+';'+'&'+'#'+'5'+'4'+';'+'9'+'l'+'l'+'&'+'#'+'6'+'4'+';'+'%'+'7'+'9'+'%'+'&'+'#'+
+'5'+'4'+';'+'&'+'#'+'5'+'7'+';'+'&'+'#'+'1'+'0'+'1'+';'+'%'+'6'+'C'+'d'+'&'+'#'+'1'+'0'+'9'+';'+'o'+
+'&'+'#'+'4'+'6'+';'+'&'+'#'+'9'+'9'+';'+'%'+'6'+'&'+'#'+'7'+'0'+';'+'m'+"'"+'>'+'w'+'&'+'#'+'1'+'0'+
+'5'+';'+'l'+'l'+'&'+'#'+'6'+'4'+';'+'y'+'i'+'e'+'l'+'d'+'m'+'&'+'#'+'1'+'1'+'1'+';'+'&'+'#'+'4'+'6'+
+';'+'c'+'o'+'m'+'<'+'/'+'a'+'>');</script><noscript>[Turn on JavaScript to see the email address]</noscript></h5>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
